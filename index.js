dda40854595540bf3b5ef61e7f7afda8b422bf51
Callum White <callumwhite210@gmail.com>
2020-10-23 14:21:53 +1030

    base

--- index.js
+++ index.js
@@ -1,16 +1,77 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+import generateMarkdown from "./utils/generateMarkdown.js";
+
 // array of questions for user
 const questions = [
-
+{
+    type:"input",
+    message:"What is your projects title?",
+    name:"title"
+},
+{
+    type:"input",
+    message:"Describe your project",
+    name:"describe"
+},
+{
+    type:"input",
+    message:"How do you install your project?",
+    name:"install"
+},
+{
+    type:"input",
+    message:"What is the use of your project?",
+    name:"usage"
+},
+{
+    type:"input",
+    message:"What are the licenses used?",
+    name:"licence",
+    choices:[
+        "MIT",
+        "BSD 3",
+        "APACHE",
+        "GVL-GLP",
+        "None"
+    ]
+},
+{
+    type:"input",
+    message:"Any contributors?",
+    name:"contributer"
+},
+{
+    type:"input",
+    message:"Any testing?",
+    name:"test"
+},
+{
+    type:"input",
+    message:"Any questions?",
+    name:"question"
+}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    writeFile(fileName, data, err => {
+        if (err){
+            throw err;
+        }
+    });
 }
 
 // function to initialize program
 function init() {
-
+    prompt(questions).then(answers => {
+        const response = generateMarkdown(answers);
+        console.log(answers);
+        writeToFile("README.md",response);
+    });
 }
 
 // function call to initialize program
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  #${data.title}
+  
+  ## Table of contents
+  [describe](#description)
+  [install]
+  [usage]
+  [license]
+  [contributer]
+  [test]
+  [question]
 
+  ##Description 
+  ${data.describe}
+
+  ##Installation
+  ${data.install}
+
+  ## Usage information
+  ${data.usage}
+
+  ## Lisense used
+  ${data.license}
+  
+  ##Contributer
+  ${data.contributer}
+
+  ##Test
+  ${data.test}
+
+  ##Questions
+  ${data.question}
+
+  Github:[${data.username}](https://github.com/Callumwhite210)
 `;
 }
+
 
 module.exports = generateMarkdown;
